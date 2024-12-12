9230f9c6d4b4ef8a5c3acf8c2b75527c08100eeb
Milind Mishra <milind.mishra4@gmail.com>
2024-12-12 20:24:48 +0530

    feat: update procy url

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://sse.milind.app",
         changeOrigin: true,
       },
     },
